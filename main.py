94fa6e0761aa1a2cd60e60681e80244e353418b8
uabharuhi <uabharuhi@gmail.com>
2018-10-20 18:09:24 +0800

    preprocessing ibm file to trans line

---
+++ main.py
@@ -0,0 +1,2 @@
+from util import  preprocessing_ibm_generated_file
+preprocessing_ibm_generated_file('./datas/data131','./datas/data131.l')
---
+++ util.py
@@ -0,0 +1,29 @@
+import json,collections
+
+
+
+def read_ibm_file(ibm_path):
+    with open(ibm_path,'r') as f:
+        d = collections.OrderedDict()
+        for l in f:
+            fields = l.rstrip().split()
+            tid,item = fields[1],fields[2]
+            d.setdefault(tid,[]).append(item)   
+    return d
+
+
+def preprocessing_ibm_generated_file(ibm_path,tar_path):
+    trans = read_ibm_file(ibm_path)
+    lines = []
+    for l in trans.values() :
+        lines.append(",".join(l))
+    dump_lines(lines,tar_path)
+
+def dump_lines(lines,path):
+    with open(path,'w') as f:
+       for line in lines:
+           f.write(line+'\n')
+
+
+def readlines(path):
+    pass
